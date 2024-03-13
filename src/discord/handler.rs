32f6ec052d745e14a41cc46a29dd764aed3c4d37
etaeleith <sabas.ll.w@gmail.com>
2024-03-13 02:27:08 -0300

    0.0.5c - pin msg

--- src/discord/handler.rs
+++ src/discord/handler.rs
@@ -7,7 +7,6 @@ use crate::data_structs::TopTen;
 use crate::db::commands::table_tournaments::get_top;
 
 pub struct Handler;
-
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
@@ -35,16 +34,37 @@ impl EventHandler for Handler {
                             .fields(fields_vec);
 
                         let builder = CreateMessage::new().embed(embed);
-                        let msg = msg.channel_id.send_message(&ctx.http, builder).await;
-                        if let Err(why) = msg {
-                            println!("Error sending message: {:?}", why);
+
+                        // Despinea el mensaje anterior
+                        if let Ok(pinned_msgs) = msg.channel_id.pins(&ctx.http).await {
+                            for pinned_msg in pinned_msgs {
+                                if let Some(embeds) = pinned_msg.embeds.first() {
+                                    if embeds.title == Some("Top 10 Usuarios".to_string()) {
+                                        if let Err(why) = pinned_msg.unpin(&ctx.http).await {
+                                            println!("Error despineando el mensaje: {:?}", why);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        match msg.channel_id.send_message(&ctx.http, builder).await {
+                            Ok(new_msg) => {
+                                if let Err(why) = new_msg.pin(&ctx.http).await {
+                                    println!("Error pineando el mensaje: {:?}", why);
+                                }
+                            }
+                            Err(why) => {
+                                println!("Error enviando el mensaje: {:?}", why);
+                            }
                         }
                     } else {
                         let builder =
                             CreateMessage::new().content("Error al obtener el top 10 de usuarios");
                         let msg = msg.channel_id.send_message(&ctx.http, builder).await;
                         if let Err(why) = msg {
-                            println!("Error sending message: {:?}", why);
+                            println!("Error enviando el mensaje: {:?}", why);
                         }
                     }
                 }
@@ -53,41 +73,7 @@ impl EventHandler for Handler {
                 }
             }
         }
-        /*  match get_last_tournament() {
-               Ok(response) => {
-                   if response.success {
-                       let top_users: Vec<TopTen> =
-                           serde_json::from_str(&response.success_description.unwrap()).unwrap();
-                       let mut fields_vec = Vec::new();
-                       for user in top_users {
-                           fields_vec.push((
-                               format!("Posición {}", user.position),
-                               format!("{}: {} puntos", user.name, user.points),
-                               false,
-                           ));
-                       }
-                       let embed = CreateEmbed::new().title("Último Torneo").fields(fields_vec);
-
-                       let builder = CreateMessage::new().embed(embed);
-                       let msg = msg.channel_id.send_message(&ctx.http, builder).await;
-                       if let Err(why) = msg {
-                           println!("Error sending message: {:?}", why);
-                       }
-                   } else {
-                       let builder = CreateMessage::new().content("Error al obtener el último torneo");
-                       let msg = msg.channel_id.send_message(&ctx.http, builder).await;
-                       if let Err(why) = msg {
-                           println!("Error sending message: {:?}", why);
-                       }
-                   }
-               }
-               Err(err) => {
-                   println!("Error al conectar a la base de datos: {:?}", err);
-               }
-           }
-        */
     }
-
     async fn resume(&self, _: Context, _: ResumedEvent) {
         println!("Bot resumed");
     }
